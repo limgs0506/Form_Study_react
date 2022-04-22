b79d78c7458b14de205282f6fda2d1870bf245a3
LimGS <limgs0506@gmail.com>
2022-04-22 10:44:25 +0900

    FEAT AuthPage로 컴포넌트 통합

--- src/components/page/auth.tsx
+++ src/components/page/auth.tsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Header from "../organs/header";
+import Main from "./../organs/main";
 import Footer from "./../organs/footer";
 
 const AuthPage: React.FC = () => {
 	return (
 		<>
 			<Header />
-			{/* <Main /> */}
+			<Main />
 			<Footer />
 		</>
 	);
