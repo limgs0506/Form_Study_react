1b2b5723ff6bc994d1bc1586fdc234f25589547c
LimGS <limgs0506@gmail.com>
2022-05-04 12:37:57 +0900

    FIX  - App.tsx import 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-import AuthPage from "./components/page/auth";
+import AuthPage from "./domain/signUp/signUp";
 
 function App() {
 	const [title, setTitle] = useState("리액트");
