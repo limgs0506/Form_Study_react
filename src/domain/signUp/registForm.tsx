f4abe3626820001d256e69fb344fec5e048975f8
LimGS <limgs0506@gmail.com>
2022-05-04 22:14:19 +0900

    FIX - registForm 컴포넌트 통합
    애초에 html 100줄도 안되는 걸 어떻게든 효율적으로 뜯어보겠다고 끙끙대느라 시간 낭비할 이유가 없었다.

--- src/domain/signUp/registForm.tsx
+++ src/domain/signUp/registForm.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import InputText from "./inputText";
-import Input from "../../components/atom/input";
-import GenderRadio from "./genderRadio";
 import styled from "styled-components";
 
-const StyledForm = styled.form`
+const Form = styled.form`
 	display: flex;
 	flex-wrap: wrap;
 	justify-content: space-between;
@@ -15,33 +12,135 @@ const StyledForm = styled.form`
 	background: hsl(155, 100%, 89%);
 	border-radius: 20px;
 `;
-const RegistForm: React.FC = () => {
+const Div = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: calc(100% / 2 - 10px);
+	#wide {
+		width: 100%;
+	#wide > textarea {
+		padding: 5px;
+		resize: none;
+	}
+`;
+const Label = styled.label`
+	font-size: small;
+	font-weight: bolder;
+	color: darkgreen;
+`;
+const Input = styled.input`
+	height: 50px;
+	width: 100%;
+	border-radius: 5px;
+	outline: none;
+	font-size: large;
+	padding-left: 10px;
+	border: 1px solid #ccc;
+	border-bottom-width: 2px;
+	transition: all 0.3s ease;
+	&:invalid {
+		border-color: red;
+	}
+	&:empty {
+		border-color: #ccc;
+	}
+	&:focus {
+		border-color: #9b59b6;
+	}
+	/* &:valid {
+		border-color: #9b59b6;
+	} */
+`;
+const DivGender = styled.div`
+	display: flex;
+	height: 50px;
+	justify-content: space-around;
+	align-items: center;
+	font-size: medium;
+`;
+const DivInputGender = styled.div`
+	& > input:checked + label {
+		font-weight: bold;
+	}
+`;
+const DivSubmit = styled.div`
+	display: flex;
+	width: 100%;
+	align-items: flex-end;
+`;
+const Submit = styled.input`
+	width: 100%;
+	height: 100%;
+	border: 0;
+	background-color: coral;
+	border-radius: 5px;
+	font-size: large;
+	&:hover {
+		background-color: chartreuse;
+	}
+	g:active {
+		background-color: crimson;
+	}
+`;
+
+const RegisthtmlForm: React.FC = () => {
 	return (
-		<StyledForm action="result" method="post" className="form">
-			<InputText type="text" name="id" holder="">
-				ID
-			</InputText>
-			<InputText type="text" name="name" holder="">
-				Name
-			</InputText>
-			<InputText type="password" name="pass" id="pass" holder="">
-				Password
-			</InputText>
-			<InputText type="password" name="pass_conf" id="pass_conf" holder="">
-				Confirm Password
-			</InputText>
-			<InputText type="date" name="birth">
-				Birth
-			</InputText>
-			<InputText type="text" name="adress">
-				adress
-			</InputText>
-
-			<GenderRadio type="radio" name="gender" />
-
-			<Input type="submit" name="submit" value="Register" />
-		</StyledForm>
+		<Form action="result" method="post" className="htmlForm">
+			<Div>
+				<Label htmlFor="id"> 아이디 </Label>
+				<Input type="text" name="id" placeholder="xx@email.com" />
+			</Div>
+			<Div>
+				<Label htmlFor="name"> 이름 </Label>
+				<Input type="text" name="name" />
+			</Div>
+			<Div>
+				<Label htmlFor="pw"> 비밀번호 </Label>
+				<Input
+					type="password"
+					name="pw"
+					placeholder="영문, 숫자 포함 8~16자"
+					id="password"
+				/>
+			</Div>
+			<Div>
+				<Label htmlFor="password"> 비밀번호 확인</Label>
+				<Input type="password" name="pass_con" id="password_confirm" />
+			</Div>
+			<Div>
+				<Label htmlFor="birth"> 출생연도 </Label>
+				<Input type="date" name="birth" />
+			</Div>
+			<Div>
+				<Label htmlFor="adress"> 주소 </Label>
+				<Input type="text" name="adress" />
+			</Div>
+			<Div id="wide">
+				<Label htmlFor="gender"> 성별 </Label>
+				<DivGender>
+					<DivInputGender>
+						<input type="radio" name="gender" value="male" id="male" />
+						<Label htmlFor="male">male</Label>
+					</DivInputGender>
+					<DivInputGender>
+						<input type="radio" name="gender" value="female" id="female" />
+						<Label htmlFor="female">female</Label>
+					</DivInputGender>
+					<DivInputGender>
+						<input type="radio" name="gender" value="etc" id="etc" />
+						<Label htmlFor="etc">etc</Label>
+					</DivInputGender>
+				</DivGender>
+			</Div>
+			<Div id="wide">
+				<Label htmlFor="bio"> 자기소개 </Label>
+				<textarea name="bio" rows={5} />
+			</Div>
+			<DivSubmit>
+				<Submit type="submit" value="회원가입" />
+			</DivSubmit>
+		</Form>
 	);
 };
 
-export default RegistForm;
+export default RegisthtmlForm;
