4b2b856bc9ac63bc70c40f60ccc4256115a4e491
LimGS <limgs0506@gmail.com>
2022-05-04 14:49:19 +0900

    FEAT - postTemplate 컴포넌트 생성

---
+++ src/templates/postTemplate.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from "react";
+import Footer from "../components/footer/footer";
+import Header from "../components/header/header";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+	flex: 1 0 auto;
+	padding: 0 10%;
+	background-color: dimgray;
+`;
+const StyledMain = styled.main`
+	flex: 1 0 auto;
+	min-height: 100%;
+	padding: 0 3rem;
+	background-color: white;
+`;
+
+type Props = {
+	children: ReactNode;
+};
+const PostTemplate: React.FC<Props> = (props) => {
+	return (
+		<>
+			<Header />
+			<StyledDiv>
+				<StyledMain>{props.children}</StyledMain>
+			</StyledDiv>
+			<Footer />
+		</>
+	);
+};
+
+export default PostTemplate;
