631a0720eaaf906019ea504f20fe1ec726e66407
LimGS <limgs0506@gmail.com>
2022-05-20 03:34:26 +0900

    FIX - <Router> element 위치 수정

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import HeaderNav from "./HeaderNav";
 import HeaderTitle from "./HeaderTitle";
-import { BrowserRouter as Router } from "react-router-dom";
 
 const Header: React.FC = () => {
 	return (
 		<header className="header_container">
-			<Router>
-				<HeaderTitle>title</HeaderTitle>
-				<HeaderNav />
-			</Router>
+			<HeaderTitle>title</HeaderTitle>
+			<HeaderNav />
 		</header>
 	);
 };
